7d5eacfac849f4b9cfaac0f37ba5adf75a3a1a8f
OMAR HEGAZI <omar.hegazy65@gmail.com>
2025-02-23 21:35:50 +0200

    2 changes by Nuxt Studio

--- app/app.config.ts
+++ app/app.config.ts
@@ -4,10 +4,10 @@ export default defineAppConfig({
     available: false,
   },
   profile: {
-    name: 'Hugo Richard',
-    job: 'Frontend Architect and Designer',
-    email: 'contact@hrcd.fr',
-    phone: '(+33) 6 21 56 22 18',
+    name: 'Omar Hegazi',
+    job: 'CEO of Gawlaat',
+    email: 'o.hegazy@gawlaat.com',
+    phone: '(+2) 01068684376',
     picture: 'https://avatars.githubusercontent.com/u/71938701?v=4',
   },
   socials: {
